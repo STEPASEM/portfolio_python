1dbfd6e62bb60a74b9bd323a8232207c2b01555b
STEPASEM <94398631+STEPASEM@users.noreply.github.com>
2025-03-10 16:28:48 +0300

    1

--- Yandex_CodeRun/1_middle_element.py
+++ Yandex_CodeRun/1_middle_element.py
@@ -4,9 +4,6 @@
 Рассмотрим три числа a, b и c. Упорядочим их по возрастанию.
 Какое число будет стоять между двумя другими?
 """
-import sys
-
-
 def main():
     a_b_c = list(map(int, input().split()))
     print(sorted(a_b_c)[1])
