d81b8918bfc98263df248c255e73028c846f8f33
STEPASEM <94398631+STEPASEM@users.noreply.github.com>
2025-07-31 16:35:54 +0300

    [+] связь данных из БД с показом на сайте(Главной страницы и страницы отдельного мороженного)

--- Yandex_course_python_developer/sprint_6/anfisa_for_friends/homepage/views.py
+++ Yandex_course_python_developer/sprint_6/anfisa_for_friends/homepage/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(
+        is_published=True, is_on_main=True  # Два в одном!
+    ).order_by('title')[0:3]
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
